6f18f6171553882d35f6a5820a7471386bb54203
settorka <amedikusettor@gmail.com>
2025-10-04 23:22:51 +0100

    api scaffoold

--- cross-chain-interactions/hyperlane/messages/backend/api/src/index.ts
+++ cross-chain-interactions/hyperlane/messages/backend/api/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import chainsRouter from "./routes/chains.js";
 import messagesRouter from "./routes/messages.js";
-
+import type { Request, Response } from "express";
 dotenv.config();
 
 const app = express();
@@ -10,14 +10,16 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// routes
+// crypto
 app.use("/chains", chainsRouter);
 app.use("/messages", messagesRouter);
 
-app.get("/", (_, res) => {
+
+app.get("/", (_: Request, res: Response) => {
     res.send("Cross-chain API up and running 🚀");
 });
 
+
 app.listen(port, () => {
-    console.log(` API running on http://localhost:${port}`);
+    console.log(`API running on http://localhost:${port}`);
 });
